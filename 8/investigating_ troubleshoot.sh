f83854866ffac6d18e7f61ea4deb89256f28a5eb
Caliari Jr <Calliari@users.noreply.github.com>
2022-02-02 18:41:54 +0000

    Create investigating_ troubleshoot.sh

---
+++ 8/investigating_ troubleshoot.sh
@@ -0,0 +1,40 @@
+investigation on the cluster set up 
+kubelet: [TYPE]
+kube-apiserver: [TYPE]
+kube-scheduler: [TYPE]
+kube-controller-manager: [TYPE]
+etcd: [TYPE]
+dns: [TYPE] [NAME]
+
+# Choices of [TYPE] are: 
+not-installed
+process
+static-pod
+pod
+
+####### Commnads
+#### find processes
+ps aux | grep kubelet 
+
+#### check manifest set up
+find /etc/systemd/system/ | grep kube
+find /etc/systemd/system/ | grep etcd
+
+
+#### check pods runnig in master and on the namespace 'kube-system'
+kubectl -n kube-system get pod -o wide | grep master1
+
+# check pods runnig that are no 'static-pod' (usually pods that ends with differents charactres but start with the same name), i.e:
+coredns-78fcd69978-mscsz                   1/1     Running   1 (48m ago)   23h
+coredns-78fcd69978-zfmcd                   1/1     Running   1 (48m ago)   23h
+
+## how they (coredns-78fcd69978-***) are managed ? 
+kubectl -n kube-system describe pod coredns-78fcd69978-zfmcd | grep Controlled
+# Controlled By:  ReplicaSet/coredns-78fcd69978 # looks like another object ending with differents charactres but start with the same name
+
+## how they (ReplicaSet/coredns-***) are managed ?
+kubectl describe ReplicaSet/coredns-78fcd69978 -n kube-system | grep Controlled
+Controlled By:  Deployment/coredns
+
+
+
